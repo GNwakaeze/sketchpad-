7f4837979536b5e7a8efc8de91a16c8755169356
GladysTTWC <thetheywhocodes@outlook.com>
2022-02-25 19:13:52 +0000

    SketchPad to my liking before adding in the colours detailed in the spec

--- main.js
+++ main.js
@@ -1,11 +1,56 @@
 const container = document.querySelector('.container');
 const gridSquare = document.querySelector('.gridSquare')
-let squareNumber = 10000;
+const header = document.querySelector('header')
 
+let btn = document.createElement('button');
+btn.textContent = "New Sketch Pad";
+btn.style.fontSize = '17px';
+document.body.insertBefore(btn, container);
+
+btn.addEventListener('click', getPad);
+
+function getPad() {
+    container.innerHTML = '';
+    
+    let userChoice = prompt('Choose the size of your new pad. (Tip: for 16x16, just type "16")');
+
+    if (userChoice == '' || userChoice > 100 || userChoice < 1) {
+        alert('you need to pick between 1 & 100. Now refersh your page');
+    } else {
+        let chosenGrid = (10 * userChoice) + 'px';
+
+        container.style.height = chosenGrid;
+        container.style.width = chosenGrid;
+
+        for (i=0; i < (userChoice * userChoice); i++) {
+            let grid = document.createElement('div');
+            grid.classList.add('gridSquare');
+            grid.addEventListener('mouseover', () => {grid.style.backgroundColor = 'grey'});
+            container.appendChild(grid);
+        }
+    } 
+}
+
+function clearPad() {
+    
+
+    /*
+    get container 
+    remove all its children 
+    then run get 
+    */
+
+    getPad();
+}
+
+
+
+/*
+let squareNumber = 16*16;
 for (i=0; i < squareNumber; i++) {
 let grid = document.createElement('div');
-grid.addEventListener('mouseover', () => {grid.style.backgroundColor = 'black'});
+grid.addEventListener('mouseover', () => {grid.style.backgroundColor = 'grey'});
 grid.classList.add('gridSquare');
-grid.setAttribute('style', 'display: flex; flex: 1 1 5px; height: 5px; /*border: 1px solid white*/');
 container.appendChild(grid);
 }
+*/
