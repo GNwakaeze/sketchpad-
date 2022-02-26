16f979ccfc31dcb14f3444377f832762b73f9be3
GladysTTWC <thetheywhocodes@outlook.com>
2022-02-26 18:28:28 +0000

    Plain sketchpad with CSS Grid

--- main.js
+++ main.js
@@ -1,56 +1,33 @@
 const container = document.querySelector('.container');
-const gridSquare = document.querySelector('.gridSquare')
-const header = document.querySelector('header')
+
+container.setAttribute('style', `display: grid; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr)`);
+
+for (i=0; i < (16*16); i++) {
+    let grid = document.createElement('div');
+    grid.setAttribute('style', 'border: 1px  solid grey')
+    grid.addEventListener('mouseover', () => {grid.style.backgroundColor = 'grey'});
+    container.appendChild(grid);
+}
 
 let btn = document.createElement('button');
-btn.textContent = "New Sketch Pad";
+btn.textContent = 'New Sketch Pad';
 btn.style.fontSize = '17px';
 document.body.insertBefore(btn, container);
 
-btn.addEventListener('click', getPad);
-
-function getPad() {
+btn.addEventListener('click', () => {
     container.innerHTML = '';
     
-    let userChoice = prompt('Choose the size of your new pad. (Tip: for 16x16, just type "16")');
+    let userChoice = prompt('choose the size of your new pad. (Tip: for 16x16, just type "16")');
 
     if (userChoice == '' || userChoice > 100 || userChoice < 1) {
-        alert('you need to pick between 1 & 100. Now refersh your page');
+        alert('you need to pick a number between 1 - 100. Please try again');
     } else {
-        let chosenGrid = (10 * userChoice) + 'px';
-
-        container.style.height = chosenGrid;
-        container.style.width = chosenGrid;
-
-        for (i=0; i < (userChoice * userChoice); i++) {
+        container.setAttribute('style', `display: grid; grid-template-columns: repeat(${userChoice}, 1fr); grid-template-rows: repeat(${userChoice}, 1fr)`);
+        for (i=0; i < (userChoice*userChoice); i++) {
             let grid = document.createElement('div');
-            grid.classList.add('gridSquare');
+            grid.setAttribute('style', 'border: 1px  solid grey')
             grid.addEventListener('mouseover', () => {grid.style.backgroundColor = 'grey'});
             container.appendChild(grid);
         }
     } 
-}
-
-function clearPad() {
-    
-
-    /*
-    get container 
-    remove all its children 
-    then run get 
-    */
-
-    getPad();
-}
-
-
-
-/*
-let squareNumber = 16*16;
-for (i=0; i < squareNumber; i++) {
-let grid = document.createElement('div');
-grid.addEventListener('mouseover', () => {grid.style.backgroundColor = 'grey'});
-grid.classList.add('gridSquare');
-container.appendChild(grid);
-}
-*/
+});
